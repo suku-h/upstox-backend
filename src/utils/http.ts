caafac582c5db03e1b9dd02fc6e1e6c63b66f3ce
werty <sukumarapptest@gmail.com>
2018-02-18 08:21:38 +0530

    added util and http functions

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,12 +1,23 @@
-import { isNull } from './util'
+import ErrorMessage from '../pojos/errormessage'
+import {
+  isEmptyString,
+  isNotNull,
+  isNull,
+  isValidEmail
+  } from './util'
+import { Response } from 'express'
 export const INVALID_REQUEST = 'Invalid Request'
 
-export function badRequest(message: String): Error {
-  return new Error('[400] ' + message)
+export function badRequest(message: string): ErrorMessage {
+  return new ErrorMessage(400, message)
 }
 
-export function notFound(message: String): Error {
-  return new Error('[404] ' + message)
+export function notFound(message: string): ErrorMessage {
+  return new ErrorMessage(404, message)
+}
+
+export function unauthorised(message: string): ErrorMessage {
+  return new ErrorMessage(401, message)
 }
 
 export function checkBadRequest(obj: any, logMessage: string) {
@@ -21,4 +32,31 @@ export function checkNotFound(obj: any, objType: string, logMessage: string) {
     console.log(logMessage)
     throw notFound(objType + ' was not found')
   }
+}
+
+export function checkValidEmail(obj: any) {
+  if (!isValidEmail(obj)) {
+    console.log('Invalid email for ' + JSON.stringify(obj))
+    throw badRequest(INVALID_REQUEST)
+  }
+}
+
+export function checkEmptyString(obj: any, logMessage: string) {
+  if (!isEmptyString(obj)) {
+    console.log(logMessage)
+    throw badRequest(INVALID_REQUEST)
+  }
+}
+
+export function returnError(err, res: Response): Response {
+  let statusCode = 500
+  if (isNotNull(err.statusCode)) {
+    statusCode = err.statusCode
+  }
+  let errStr = err.toString()
+  if (isNotNull(err.error)) {
+    errStr = err.error.toString()
+  }
+
+  return res.status(statusCode).json(errStr)
 }
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -8,3 +8,18 @@ export function isNull(obj: any): boolean {
 export function isNotNull(obj: any): boolean {
   return !isNull(obj)
 }
+
+export function isEmptyString(obj: any): boolean {
+  if (isNotNull(obj) && typeof obj === 'string' && obj.trim().length > 0) {
+    return false
+  }
+  return true
+}
+
+export function isValidEmail(obj: any): boolean {
+  if (!isEmptyString(obj)) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return regex.test(obj)
+  }
+  return false
+}
